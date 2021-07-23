eafe5140d7fb5a306f002efd8782bbc7ff142cf7
Magesh Kuppan <tkmagesh77@gmail.com>
2021-07-23 12:57:26 +0530

    Promise & async await

--- 00-async/script.js
+++ 00-async/script.js
@@ -13,19 +13,55 @@
     }
     window['addSyncClient'] = addSyncClient;
 
-    function addAsync(x,y){
+    function addAsync(x,y, callback){
         console.log(`   [@service] processing ${x} and ${y}`)
         setTimeout(function(){
             let result = x + y;
             console.log(`   [@service] returning result`)
-            return result;
+            callback(result);
         }, 4000);
     }
 
     function addAsyncClient(x,y){
         console.log(`[@client] invoking the service`)
-        let result = addAsync(x,y);
-        console.log(`[@client] result = ${result}`)
+        addAsync(x,y, function(result){
+            console.log(`[@client] result = ${result}`)
+        });
     }
     window['addAsyncClient'] = addAsyncClient;
+
+    function addAsyncPromise(x,y){
+        console.log(`   [@service] processing ${x} and ${y}`)
+        const p = new Promise(function(resolveFn, rejectFn){
+            setTimeout(function(){
+                let result = x + y;
+                console.log(`   [@service] returning result`)
+                resolveFn(result);
+            }, 4000);
+        });
+        return p;
+    }
+
+    window['addAsyncPromise'] = addAsyncPromise;
+
+   
+    /* function addAsyncPromiseClient(x,y){
+        console.log(`[@client] invoking the service`)
+        var p = addAsyncPromise(100,200)
+        p.then(function(result){
+            console.log(`[@client] result = ${result}`)
+        })
+    }
+ */
+
+    async function addAsyncPromiseClient(x,y){
+        console.log(`[@client] invoking the service`)
+        var result = await addAsyncPromise(100,200)
+        console.log(`[@client] result = ${result}`)
+        let doubleResult = result * 2;
+        return doubleResult;
+    }
+
+    window['addAsyncPromiseClient'] = addAsyncPromiseClient;
+
 })()
