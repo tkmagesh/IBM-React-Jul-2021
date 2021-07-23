4c11ac4baf8446fdc538c51c9bccbe754cd049f5
Magesh Kuppan <tkmagesh77@gmail.com>
2021-07-23 11:03:00 +0530

    associating projects with bugs

--- bug-tracker-app/src/bugs/components/bug-edit.jsx
+++ bug-tracker-app/src/bugs/components/bug-edit.jsx
@@ -4,8 +4,14 @@ const BugEdit = ({addNew}) => {
     const [newBugName, setNewBugName] = useState('');
     return (
         <section className="edit">
-            <label htmlFor="">Bug Name :</label>
+            <label htmlFor="">Bug Name : </label>
             <input type="text" onChange={evt => setNewBugName(evt.target.value)} />
+            <label htmlFor=""> Project : </label>
+            <select>
+                <option></option>
+                <option></option>
+                <option></option>
+            </select>
             <input type="button" value="Add New" onClick={() => addNew(newBugName)} />
         </section>
     )
